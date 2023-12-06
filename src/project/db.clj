e130c28e5fbbc26b021e8739f591c9391865523d
Radha M <radha.m@tavant.com>
2023-12-06 16:21:43 +0530

    updated CRUD operation

--- src/project/db.clj
+++ src/project/db.clj
@@ -1,42 +1,29 @@
 (ns project.db
-
-  (:require [clojure.java.jdbc :as jdbc]
-            ))
-
-;; (def db-spec
-
-;;   {:classname "org.h2.Driver"
-
-;;    :subprotocol "h2:mem"
-
-;;    :subname "test-db"})
-
-
+  (:require [clojure.java.jdbc :as jdbc]))
 
 (def db-spec
-    {:subprotocol "mysql"
-     :subname "//127.0.0.1:3306/TESTDB1"
-     :user "root"
-     :password "Sathyabama@123"}
-  )
-
+  {:subprotocol "mysql"
+   :subname "//127.0.0.1:3306/TESTDB1"
+   :user "root"
+   :password "Sathyabama@123"})
 
 (defn create-table []
-  (println "create table func")
-  (jdbc/execute! db-spec ["CREATE TABLE TEST_TABLE (ID INT AUTO_INCREMENT PRIMARY KEY, NAME VARCHAR(255))"]))
+  (jdbc/execute! db-spec ["CREATE TABLE IF NOT EXISTS TEST_TABLE (ID INT AUTO_INCREMENT PRIMARY KEY, NAME VARCHAR(255))"]))
 
 (defn insert-data [name]
-(println "insert data func")
-(jdbc/with-db-connection [db-conn db-spec]
-
-(jdbc/insert! db-conn
-
-:TEST_TABLE
-
-{:name name})))
+  (jdbc/with-db-connection [db-conn db-spec]
+    (jdbc/insert! db-conn :TEST_TABLE {:name name})))
 
 (defn query-data []
-(println "query data func")
-(jdbc/with-db-connection [db-conn db-spec]
+  (jdbc/with-db-connection [db-conn db-spec]
+    (jdbc/query db-conn ["SELECT * FROM TEST_TABLE"])))
 
-(jdbc/query db-conn ["SELECT * FROM TEST_TABLE"])))
+(defn update-data [id name]
+  (println "update data func")
+  (jdbc/with-db-connection [db-conn db-spec]
+    (jdbc/execute! db-conn ["UPDATE TEST_TABLE SET NAME=? WHERE ID=?" name id])))
+
+(defn delete-data [id]
+  (println "delete data func")
+  (jdbc/with-db-connection [db-conn db-spec]
+    (jdbc/execute! db-conn ["DELETE FROM TEST_TABLE WHERE ID=?" id])))
--- src/project/handler.clj
+++ src/project/handler.clj
@@ -1,71 +1,3 @@
-;; (ns project.handler
-;;   (:require [clojure.string :as str]
-;;             [compojure.core :refer [defroutes GET POST]]
-;;             [compojure.route :as route]
-;;             [project.db :as db] 
-;;             [ring.middleware.defaults :refer [site-defaults wrap-defaults]] 
-;;             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
-;;             [ring.util.anti-forgery :refer [anti-forgery-field]]
-;;             [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]
-;;             [ring.middleware.nested-params :refer [wrap-nested-params]]
-;;             [ring.middleware.params :refer [wrap-params]]
-;;             [ring.middleware.session :refer [wrap-session]] 
-;;             [ring.util.response :as response]))
-
-
-;; (defn home-page []
-
-;;   (str "<h1>Welcome to Clojure Web DB App</h1>"
-;;        "<a href='/insert'>Insert Data</a><br/>"
-;;        "<a href='/data'>View Data</a>"))
-
-
-
-
-;; (defroutes app-routes
-;;   (GET "/" [] "Hello World")
-;;   (GET "/home" [] (home-page))
-;;   (GET "/insert" [] (str "<h2>Insert Data</h2>"
-;;                          "<form action='/insert' method='post'>"
-;;                          (anti-forgery-field)
-;;                          "Name: <input type='text' name='name'><br/>"
-;;                          "<input type='submit' value='Submit'></form>"))
-;;   (POST "/insert" [params] (do (db/insert-data (:name params)) 
-;;                                (response/redirect "/")))
-;;   (route/not-found "Not Found"))
-
-;; (GET "/data" [] (str "<h2>View Data</h2>"
-;;                       (->> (db/query-data)
-;;                            (map #(str "ID: " (:id %) ", Name: " (:name %)))
-;;                            (str/join "<br/>"))))
-
-
-;; (def app
-;;   (-> app-routes
-;;       (wrap-session)
-;;       (wrap-params)
-;;       (wrap-nested-params)
-;;       (wrap-keyword-params)
-;;       (wrap-defaults site-defaults)
-;;       (wrap-anti-forgery)
-;;       )) ; Add CSRF middleware appropriately
-
-
-
-;; ;; (def app
-;; ;;   (wrap-defaults app-routes site-defaults))
-
-
-;; ;; (def app
-;; ;;   (-> app-routes
-;; ;;       (wrap-keyword-params)
-;; ;;       (wrap-nested-params)
-;; ;;       (wrap-params)
-;; ;;       (wrap-session)
-;; ;;       (wrap-defaults site-defaults)))
-
-
-
 (ns project.handler
   (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :as route]
@@ -80,56 +12,86 @@
             [ring.middleware.session :refer [wrap-session]]
             [project.db :as db]))
 
-
-
-
 (defn home-page []
   (str "<h1>Welcome to Clojure Web DB App</h1>"
        "<a href='/insert'>Insert Data</a><br/>"
-       "<a href='/data'>View Data</a>"))
+       "<a href='/data'>View Data</a><br/>"
+      ;;  "<a href='/delete/56'>Delete Data</a><br/>"
+      ;;  "<a href='/update/57'>Update Data</a><br/>" 
+       "<a href='/update'>update Data</a><br/><br/>"
+       "<a href='/delete'>Delete Data</a><br/>"
+       ))
+
+;; (defn delete-form [id]
+;;   (str "<h2>Delete Data</h2>"
+;;        "<form action=\"/delete/" id "\" method=\"post\">"
+;;        (anti-forgery-field)
+;;        "<input type=\"submit\" value=\"Confirm Delete\"></form>"))
+
+;; (defn update-form [id]
+;;   (str "<h2>Update Data</h2>"
+;;        "<form action=\"/update/" id "\" method=\"post\">"
+;;        (anti-forgery-field)
+;;        "New name: <input type=\"text\" name=\"name\"><br/>"
+;;        "<input type=\"submit\" value=\"Update\"></form>"))
 
 (defroutes app-routes
   (GET "/" [] (home-page))
   (GET "/insert" [] (str "<h2>Insert Data</h2>"
-                         "<form action='/insert' method='post'>"
+                         "<form action='/insert' method='post'>" 
                          (anti-forgery-field)
                          "Name: <input type='text' name='name'><br/>"
-                         "<input type='submit' value='Submit'></form>")) 
+                         "<input type='submit' value='Submit'></form>"))
 
-  ;; (POST "/insert" [params] (do (db/insert-data (:name params))
-  ;;                              (println params)
-  ;;                              (response/redirect "/")))
-
-  ;; (POST "/insert" [params]
-  ;;   (let [form-name (:name params)] 
-  ;;     (do
-  ;;       (db/insert-data form-name) 
-  ;;       (println params) 
-  ;;       (response/redirect "/")))) 
-
-  ;; (POST "/insert" [params]
-  ;;   (let [form-name (:name params)]
-  ;;     (db/insert-data form-name)
-  ;;     (println "Data is likely inserted now")
-  ;;     (response/redirect "/")))
-
+  (POST "/insert" [name]
+    (db/insert-data name)
+    (response/redirect "/"))
   
- (POST "/insert" [name]
-   (println name) 
-   (db/insert-data name) 
-   (response/redirect "/")) 
 
+  (GET "/update" [] (str "<h2>Update Data</h2>"
+                         "<form action='/update' method='post'>"
+                         (anti-forgery-field)
+                         "ID: <input type='text' name='id'><br/>"
+                         "New Name: <input type='text' name='new_name'><br/>"
+                         "<input type='submit' value='Update'></form>"))
+  
+  (POST "/update" [id new_name]
+    (db/update-data id new_name)
+    (response/redirect "/"))
+
+  (GET "/delete" [] (str "<h2>Delete Data</h2>"
+                         "<form action='/delete' method='post'>"
+                         (anti-forgery-field)
+                         "ID: <input type='text' name='id'><br/>"
+                         "<input type='submit' value='Delete'></form>"))
+  
+  (POST "/delete" [id]
+    (db/delete-data id)
+    (response/redirect "/"))
+
+
+
+  (GET "/data" []
+    (str "<h2>View Data</h2>"
+         (->> (db/query-data)
+              (map #(str "ID: " (:id %) ", Name: " (:name %)))
+              (str/join "<br/>"))))
+
+;;  (GET "/delete/:id" [id] (delete-form id))
+;;  (POST "/delete/:id" [id]
+;;    (db/delete-data (Integer/parseInt id))
+;;    (response/redirect "/"))
  
-  (GET "/data" [] (str "<h2>View Data</h2>"
-                       (->> (db/query-data)
-                            (map #(str "ID: " (:id %) ", Name: " (:name %)))
-                            (str/join "<br/>"))))
-    (route/not-found "Not Found")
-  )
-;; (db/insert-data "aswini")
+;;  (GET "/update/:id" [id] (update-form id))
+;;  (POST "/update/:id" [id name]
+;;    (db/update-data (Integer/parseInt id) name)
+;;    (response/redirect "/"))
+
+  (route/not-found "Not Found"))
 
 
-   (def app
+
+(def app
   (-> app-routes
       wrap-params
       wrap-nested-params
@@ -137,16 +99,3 @@
       wrap-session
       wrap-anti-forgery
       (wrap-defaults site-defaults)))
-   
-;; (def app
-;;   (-> app-routes
-;;       wrap-keyword-params
-;;       wrap-nested-params
-;;       wrap-params
-;;       wrap-session
-;;       wrap-anti-forgery
-;;       (wrap-defaults site-defaults)))
-
-
-
-
